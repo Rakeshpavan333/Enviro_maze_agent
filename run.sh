61d4e3c9bc814c02757e2edf6a03ec6a7792fcfb
Rakesh Pavan <32369780+Rakeshpavan333@users.noreply.github.com>
2022-03-18 02:49:06 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# The main shell script to start the project. The order of execution is as follows:
+# First we compile the maze generator, and run it. The maze generator module writes static elements to the 'config.json' file. 
+# After that, we run the makefile which compiles the agent (Robot) code and start the enviro simulator. 
+
 g++ -std=c++17 -Wno-psabi maze_gen.cc maze_gen.h -o run
 ./run 
 make
